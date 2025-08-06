bc5b7ab191ec896bf8561db7c7a046430c892d84
Victoor <victoraju239932@gmail.com>
2025-08-06 02:16:21 -0600

    Cambio

--- componentes/banner/banner.js
+++ componentes/banner/banner.js
@@ -7,7 +7,7 @@ export function banner (){
     div.appendChild(span);
 
     let aBoton = document.createElement('a');
-    aBoton.href = "/pages/login/login1.html";
+    aBoton.href = "pages/login/login1.html";
     aBoton.textContent = "SHOP NOW";
     aBoton.className = "div-boton";
     div.appendChild(aBoton);
